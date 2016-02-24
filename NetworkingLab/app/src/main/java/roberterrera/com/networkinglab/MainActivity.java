754e156019e58e6e023b89356d6d7a4405659b6e
roberrera <roberterrera@gmail.com>
2016-02-24 17:23:04 -0500

    Setup of layout objects

--- NetworkingLab/app/src/main/java/roberterrera/com/networkinglab/MainActivity.java
+++ NetworkingLab/app/src/main/java/roberterrera/com/networkinglab/MainActivity.java
@@ -2,12 +2,34 @@ package roberterrera.com.networkinglab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mCereal, mChocolate, mTea;
+    ListView mListView;
+    ArrayList<String> mArrayList;
+    ArrayAdapter<String> mArrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCereal = (Button) findViewById(R.id.button_cereal);
+        mChocolate = (Button) findViewById(R.id.button_chocolate);
+        mTea = (Button) findViewById(R.id.button_tea);
+        mListView = (ListView) findViewById(R.id.listview);
+        mArrayList = new ArrayList<String>();
+        mArrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mArrayList);
+        mListView.setAdapter(mArrayAdapter);
+
+        
+
     }
 }
